c9b3cc979e867046ba7d1d320bd14ec379a7cea3
Ella R <ryukhinaella@gmail.com>
2023-02-01 21:13:32 -0800

    Points, comments are added to each story

--- components/Story.js
+++ components/Story.js
@@ -1,4 +1,23 @@
 export default function Story(story) {
-    console.log(story)
-    return `<div class="story">${story.title}</div>`
+    return `
+    <div class="story">
+        <div>
+            <span class="gray">${story.index}</span>
+            <span class="upvote">▲</span>
+            <a href=${story.url}">${story.title}</a>
+            <span>${story.domain}</span>
+        </div>
+        <div class="gray">
+            ${story.points} points by ${story.user} ${story.time_ago}
+            |
+            <a href="#/item?id=${story.id}">
+                ${story.comments_count} comments
+            </a>
+            | 
+            <span class="favorite">
+            <img class="heart" src="">
+            Add to Favorites
+            </span>
+        </div>
+    </div>`
 }
